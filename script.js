e586c293621b943f202771b60099f52a0523f351
Randira <204060028.mhs@stmikjabar.ac.id>
2023-04-08 20:34:36 +0700

    call atribute, elements, id, class

---
+++ script.js
@@ -0,0 +1,12 @@
+let yourName = document.getElementById("nameinput");
+let male = document.getElementById("g-male");
+let female = document.getElementById("g-female");
+let height = document.getElementById("height");
+let weight = document.getElementById("weight");
+let resultComment = document.querySelector(".comment");
+
+popupContent = document.querySelector(".popup-content");
+popupText = document.querySelector("#popupText");
+let popup = document.getElementById("myPopup");
+let spanClose = document.getElementsByClassName("close")[0];
+
